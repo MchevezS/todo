eb2663393224b32321b3ab1ad4e7932b472f63a1
MchevezS <168207997+MchevezS@users.noreply.github.com>
2024-06-25 13:49:25 -0600

    arreglo en listaddo de tarea

--- src/Componentes/ListadoTarea.jsx
+++ src/Componentes/ListadoTarea.jsx
@@ -4,15 +4,32 @@ import ListaDeTarea from "./ListaDeTareas"
 const ListadoTareas = ({lista})=>{
     return(
         <>
-            {lista.map((tarea)=>(
-                <div>
+            {lista && lista.map((tarea) => (
+                <div key={tarea.id}>
                     <input type="checkbox"/>
                     <p>{tarea.titulo}</p>
-                    <button onClick={()=>eliminarTarea(tarea.id)}>Eliminar tarea</button>
+//                     <button onClick={() => eliminarTarea(tarea.id)}>Eliminar tarea</button>
                 </div>
             ))}
         </>
     )
 }
 
-export default ListadoTareas
+export default ListadoTareas;
+// import React from 'react';
+
+// const ListadoTareas = ({ lista }) => {
+//     return (
+//         <>
+//             {lista && lista.map((tarea) => (
+//                 <div key={tarea.id}>
+//                     <input type="checkbox" />
+//                     <p>{tarea.titulo}</p>
+//                     <button onClick={() => eliminarTarea(tarea.id)}>Eliminar tarea</button>
+//                 </div>
+//             ))}
+//         </>
+//     );
+// }
+
+// export default ListadoTareas;
